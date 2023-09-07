8cdd19667dc2952d858750da2fd2ce2966571e9f
Noel Reeds <noelreeds01@gmail.com>
2023-09-07 16:42:35 -0700

    A C program that prints out a string

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This ia C program.
+ * The program prints a string.
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}

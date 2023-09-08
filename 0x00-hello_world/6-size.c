0fc8f690767761d4f34f2d39414634de9f91900c
Noel Reeds <noelreeds01@gmail.com>
2023-09-07 18:42:54 -0700

    A C program that prints the various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stadio.h>
+/**
+ * Main - The entry point of a C program.
+ *
+ * Return - Returns the sizes of various types of data types.
+ */
+int main(void)
+	int i;
+	char c;
+	long int l;
+	long long int d;
+	float f;
+{
+	puts("Size of a int: %lu byte(s)", sizeof(i));
+	puts("Size of a char: %lu byte(s)", sizeof(c));
+	puts("Size of a long int: %lu byte(s)", sizeof(l));
+	puts("Size of a long long int: %lu byte(s)", sizeof(d));
+	puts("Size of a float: %lu byte(s)", sizeof(f));
+	return (0);
+}

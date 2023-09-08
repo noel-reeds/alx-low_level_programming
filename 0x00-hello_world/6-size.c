94f7c7b16af98a2d79f85c28651b0015f013cf0a
Noel Reeds <noelreeds01@gmail.com>
2023-09-08 05:51:10 -0700

    A C program that prints the various data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,12 @@
 /**
  * main - entry point of a C program.
  *
- * Return: Returns 0 for success..
+ * Return: Returns 0 for success.
  */
 int main(void)
 {
-	float f;
-	long long int d;
-	long int c;
-	char b;
-	int i;
-
 	printf("Size of a char: %lu byte(s)\n", sizeof(b));
-	printf("Size of a int: %lu byte(s)\n", sizeof(i));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));

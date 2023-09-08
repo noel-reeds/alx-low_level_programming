270e9d8fb13412726826efbad36b24e2349b9c10
Noel Reeds <noelreeds01@gmail.com>
2023-09-07 17:27:55 -0700

    A C program that prints out a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 /**
- * Program: This is a C program.
- *
- * The program prints a string.
- *
- * Main: Entry point of a C program..
+ * Main: Entry point of a C program.
  *
  * Return: Returns zero of main.
- *
  */
 int main(void)
 {

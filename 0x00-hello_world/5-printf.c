5db2f87c80e1067f27b16479d96db278dc58a917
Noel Reeds <noelreeds01@gmail.com>
2023-09-07 17:48:09 -0700

    A C program emphasizes on grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main: The entry point for all C programs.
+ *
+ * Return: Returns 0 for success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

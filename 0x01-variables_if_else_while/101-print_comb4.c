68918c82ff7984a8f47aaab11e74e3cca13b8ead
Noel Reeds <noelreeds01@gmail.com>
2023-09-11 08:04:28 -0700

    Prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,19 +10,19 @@ int main(void)
 
 	for (d1 = 0; d1 < 8; d1++)
 	{
-		for (d2 = 1; d2 < 9; d2++)
+		for (d2 = d1 + 1; d2 < 9; d2++)
 		{
-			for (d3 = 2; d3 < 10; d3++)
+			for (d3 = d2 + 1; d3 < 10; d3++)
 			{
 			putchar((d1 % 10) + '0');
 			putchar((d2 % 10) + '0');
 			putchar((d3 % 10) + '0');
 
-			putchar(',');
-			putchar(' ');
-
 			if (d1 == 7 && d2 == 8 && d3 == 9)
 			continue;
+
+			putchar(',');
+			putchar(' ');
 			}
 		}
 	}

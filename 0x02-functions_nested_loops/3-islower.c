544079bdd99af39b7d68e032ed509fefb799bffb
Noel Reeds <noelreeds01@gmail.com>
2023-09-12 15:38:39 -0700

    Checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,11 @@
 /**
 * _islower = entry point into the program.
 *
-*Return: Return 1 for lower case and 0 for uppercase.
+*@c: The character to print
 *
+*Return: Return 1 for lower case and 0 for uppercase.
+
+*@c: The character to print.
 */
 int _islower(int c)
 {

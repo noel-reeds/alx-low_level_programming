cc78c6fbeae5d675edba20c8d9b5c08766f80d0a
Noel Reeds <noelreeds01@gmail.com>
2023-09-14 07:32:04 -0700

    Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
+* jack_bauer - prints every minute of the day.
 *
-*
-*
-*
+*@void - does not return a value.
 *
 */
 void jack_bauer(void)
@@ -11,14 +10,16 @@ void jack_bauer(void)
 	int min;
 	int hr;
 
-	for (hr = 0; hr < 23; hr++)
+	for (hr = 0; hr < 24; hr++)
 	{
-		for (min = 0; min < 59; min++)
+		for (min = 0; min < 60; min++)
 		{
-			_putchar(min + '0');
+			_putchar(hr / 10 + '0');
+			_putchar(hr % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
 		}
-		_putchar(hr + '0');
-		_putchar(':');
-		_putchar('\n');
 	}
 }

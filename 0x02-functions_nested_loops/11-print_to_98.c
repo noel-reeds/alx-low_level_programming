87ee80ddc824b4251628ffc16ae54b1ecd6a9332
Noel Reeds <noelreeds01@gmail.com>
2023-09-14 09:08:17 -0700

    Prints all natural numbers from n to 98, followed by a new line

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*
+*
+*
+*
+*/
+void print_to_98(int n)
+{
+	for (n = -97; n <= 98; n++)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+		printf(",");
+		printf(" ");
+		}
+	}
+	printf("\n");
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+*
+*
+*
+*
+*
+*/
+void times_table(void)
+{
+	int i;
+
+	for i range(10)
+	{
+		for (i = 0; i < 9; i++)
+		{
+			for (i = 0; i < 18; i =+ 2)
+			{
+				for (i = 0; i < 27; i =+ 3)
+				{
+					for (i = 0; i < 36; i =+ 4)
+					{
+						for (i = 0; i < 45; i =+ 5)
+						{
+							for (i = 0; i < 54; i =+ 6)
+							{
+								for (i = 0; i < 63; i =+ 7)
+								{
+									for (i = 0; i < 72; i =+ 8)
+									{
+										for (i = 0; i < 81; i =+ 9)
+										{
+											_putchar(0);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}

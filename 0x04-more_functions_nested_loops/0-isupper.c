a606ece7fd8f6e4b5c2eac5a25432a0e2c6836dd
Noel Reeds <noelreeds01@gmail.com>
2023-09-16 09:52:13 -0700

    To pick up later

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*
+*
+*
+*
+*
+*/
+int _isupper(int c)
+{
+	if ( )
+	{
+		c = 'a';
+		_putchar(0 + '0');
+	}
+	else
+	{
+		c = 'A';
+		_putchar(1 + '0');
+	}
+	_putchar('\n');
+	return (0);
+}

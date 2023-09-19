aacb0571788e5e7e66ad4ab69a429c37e6b7efdd
Noel Reeds <noelreeds01@gmail.com>
2023-09-19 22:23:08 +0000

    Prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,11 @@
   */
 int _strlen(char *s)
 {
-	int i;
+	int index;
 	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (index = 0; s[index] != '\0'; index++)
 		count++;
 
-	return (count);
+	return (count);::
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* _strlen - returns length of string.
+*
+*@arr: parameter to print.
+*
+*Return: Always void.
+*/
+int _strlen(char *arr)
+{
+	int index;
+	int count;
+
+	count = 0;
+	for (index = 0; arr[index] != '\0'; index++)
+	{
+		count++;
+	}
+	return (count);
+}
+/**
+* print_rev - prints string in reverse.
+*
+*@s: parameter
+*/
+void print_rev(char *s)
+{
+	int index;
+	int max;
+
+	max = _strlen(s);
+	for (index = max; index >= 0; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}

0636e9ece3c0b817e0fa582df093cd20e8cd8002
Noel Reeds <noelreeds01@gmail.com>
2023-09-25 22:51:26 +0000

    Locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strchr - locates a character in a string.
+  *
+  *@s: string to search character from.
+  *
+  *@c: character to be checked
+  *
+  *Return: return NULL if no character in string.
+  */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (s[index] == c)
+		{
+			return (&s[index]);
+		}
+	}
+	return (NULL);
+}

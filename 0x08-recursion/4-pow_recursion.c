567ec33749289fb1d8302d4c304a4582f9500f74
Noel Reeds <noelreeds01@gmail.com>
2023-09-27 21:21:39 +0000

    Returns the pow to integers.

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <math.h>
+/**
+  * _pow_recursion - returns the power of integer.
+  *
+  *@x: base.
+  *
+  *@y: exponent
+  *
+  *Return: returns the power.
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}

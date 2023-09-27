5213217e9abac6e73cf3a13b5d51920c602af3c7
Noel Reeds <noelreeds01@gmail.com>
2023-09-27 18:24:06 +0000

    Puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _puts_recursion - prints a string to stdout.
+  *
+  *@s: string to output to stdout.
+  *
+  */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}

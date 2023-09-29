54f5fa4a602414c9fad3ef3e6fbccccb16d51692
Noel Reeds <noelreeds01@gmail.com>
2023-09-29 00:50:54 +0000

    Checks for a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,38 @@
 #include "main.h"
-#include <math.h>
 /**
+  * prime_num - checks for a prime number.
   *
+  *@n: parameter to be checker for a prime.
   *
+  *@num: parameter to check again n.
   *
-  *
+  *Return: returns 1 if prime and 0 if not.
   */
-int is_prime_number(int n)
+int prime_num(int n, int num)
 {
-	sq = sqrt(n);
-
 	if (n < 2)
 	{
 		return (0);
 	}
-	if (n == 2)
+	else if (n % num == 0)
+	{
+		return (0);
+	}
+	else if (num * num > n)
 	{
 		return (1);
 	}
-	if (n % 2 
+	return (prime_num(n, num + 1));
+}
+
+/**
+  *is_prime_number - checks for prime.
+  *
+  *@n: parameter to check for prime.
+  *
+  *Return: return prime number.
+ */
+int is_prime_number(int n)
+{
+	return (prime_num(n, 2));
 }

0399d53c7d5373bb6f3ed05036f0f88c4ae11239
Noel Reeds <noelreeds01@gmail.com>
2023-09-30 16:34:31 +0000

    Program to print my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - entry point of a C program.
+  *
+  *@argc: argument count.
+  *
+  *@argv: pointer to array of strings.
+  *
+  *Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	int __attribute__ ((unused)) myargc = argc;
+	int count = 0;
+
+	printf("%s\n", argv[count]);
+	return (0);
+}

e99553842c5adc1ec27626170270b721425299cb
Noel Reeds <noelreeds01@gmail.com>
2023-10-01 16:03:50 +0000

    Adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,7 @@
   */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 	int sum;
 
 	sum = 0;
@@ -20,15 +20,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] >= '0' && *argv[i] <= '9')
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				sum += atoi(argv[i]);
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}

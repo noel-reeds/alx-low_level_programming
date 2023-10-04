e2a7a19bb226f6fdd4e5da35148455a2445f5791
Noel Reeds <noelreeds01@gmail.com>
2023-10-04 16:32:29 +0000

    Returns a pointer to a newly allocated memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+  * _strdup - duplicates a str.
+  *
+  *@str: str to duplicate.
+  *
+  *Return: Always 0.
+  */
+char *_strdup(char *str)
+{
+	size_t len = strlen(str);
+	char *dup_str = malloc(sizeof(len + 1));
+	if (dup_str != NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		strcpy(dup_str, str);
+	}
+	return (dup_str);
+}

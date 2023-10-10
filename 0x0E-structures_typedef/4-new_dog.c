a721b102ef41e8de1974d856a54c74c0b40ffc2e
Noel Reeds <noelreeds01@gmail.com>
2023-10-10 22:09:08 +0000

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,11 +12,13 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	new_dog = malloc(sizeof((name) + (owner)));
+	dog_t *new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	strcpy(new_dog, dog_t);
-	return (0);
+	new_dog->name = strdup(name);
+	new_dog->age = age;
+	new_dog->owner = strdup(owner);
+	return (new_dog);
 }

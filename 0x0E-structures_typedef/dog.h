32e0749b6f949e59c55b4dc2bf3a45ae65e618d4
Noel Reeds <noelreeds01@gmail.com>
2023-10-10 16:40:41 +0000

    Defines a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  * struct dog - details of a dog.
+  *
+  *@name: name of dog.
+  *
+  *@age: age of dog.
+  *
+  *@owner: owner of dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif /* _DOG_H_ */

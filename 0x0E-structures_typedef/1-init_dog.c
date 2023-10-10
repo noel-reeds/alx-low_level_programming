444b4f3147e5b66486189b42b0092515f86edcdb
Noel Reeds <noelreeds01@gmail.com>
2023-10-10 18:41:09 +0000

    Initializes variable of data type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+  * init_dog -intializes dog details.
+  *
+  *@d:
+  *
+  *@name:
+  *
+  *@age:
+  *
+  *@owner:
+  *
+  *Return:
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+}
+

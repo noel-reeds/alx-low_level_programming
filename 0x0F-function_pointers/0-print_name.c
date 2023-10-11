819539e6927a7281831d90e56a6028b250d20c03
Noel Reeds <noelreeds01@gmail.com>
2023-10-11 19:36:14 +0000

    Function pointers as args

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
   * print_name - prints a name.
@@ -11,5 +12,9 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+	{
+		return;
+	}
 	f(name);
 }

b59eafa405259e58223f303afbdc3a5e2f51c643
Noel Reeds <noelreeds01@gmail.com>
2023-10-15 23:40:54 +0000

    Encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,30 @@
+#include "main.h"
+/**
+  * rot13 - encodes a string using rot13.
+  *
+  *@str: str to be encoded.
+  *
+  *Return: Return a pointer to an encoded str.
+  */
+char *rot13(char *str)
+{
+	int index;
+	int mn;
+	char *format;
+	char *formatt;
 
+	format = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	formatt = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		for (mn = 0; format[mn] != '\0'; mn++)
+		{
+			if (str[index] == format[mn])
+			{
+				str[index] = formatt[mn];
+				break;
+			}
+		}
+	}
+	return (str);
+}

48a19fbb97bd956b34dff9fd7e7f8f1dd1b6cc49
Noel Reeds <noelreeds01@gmail.com>
2023-10-24 18:05:44 +0000

    Frees memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+  * free_list - frees memory allocated.
+  *
+  *@head: first node of a linkedlist.
+  *
+  *Return: Always 0.
+  */
+void free_list(list_t *head)
+{
+	free(linkedlist);
+}

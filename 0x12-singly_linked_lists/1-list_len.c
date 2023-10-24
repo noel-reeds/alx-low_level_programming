a91c9b04ca90a084037dc6d590d931ee03a0704e
Noel Reeds <noelreeds01@gmail.com>
2023-10-24 15:31:03 +0000

    A solution to print number of elements in a linkedlist

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,3 +7,13 @@
   *Return: return number of elements in a linkedlist.
   */
 size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

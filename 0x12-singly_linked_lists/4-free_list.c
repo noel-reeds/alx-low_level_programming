cada7b1c6eed0771e8d3ca36aa896904779b1ac5
Noel Reeds <noelreeds01@gmail.com>
2023-10-24 18:22:47 +0000

    Frees memory

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,5 +8,13 @@
   */
 void free_list(list_t *head)
 {
-	free(linkedlist);
+	list_t *current = head;
+
+	while (current != NULL)
+	{
+		list_t *next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
 }

92b2f131a939cab212e645f7f244298306daccb4
Noel Reeds <noelreeds01@gmail.com>
2023-10-24 23:57:11 +0000

    Frees memory and sets *head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  * free_listint - frees memory.
+  *
+  *@head: first node of a linked list.
+  *
+  *Return: Always 0.
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *aux = current;
+
+		current = current->next;
+		free(aux);
+	}
+	*head = NULL;
+}

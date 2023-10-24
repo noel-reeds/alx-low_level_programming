8dd1827fc5f47a6342c25325f04940940f87da29
Noel Reeds <noelreeds01@gmail.com>
2023-10-24 16:55:26 +0000

    Add a node at the beginning of a linkedlist

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+  * add_node - adds a node at the beginning of a linked list.
+  *
+  *@head: first node of a linked list.
+  *
+  *@str: refers to the linked list.
+  *
+  *Return: Returns a linkedlist with a newly added node.
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *linkedlist = malloc(sizeof(list_t));
+	if (linkedlist == NULL)
+	{
+		return (NULL);
+	}
+	linkedlist->str = strdup(str);
+	if (linkedlist == NULL)
+	{
+		return (NULL);
+	}
+	linkedlist->len = strlen(str);
+	linkedlist->next = *head;
+	*head = linkedlist;
+	return(linkedlist);
+}

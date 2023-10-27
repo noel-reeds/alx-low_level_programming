9d5c5852c16c57e1f59c3659a8b2f739af886f47
Noel Reeds <noelreeds01@gmail.com>
2023-10-27 23:37:34 +0000

    Partially solved task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_binary - converts integer to binary.
+  *
+  *@n: integer to be converted.
+  *
+  */
+void print_binary(unsigned long int n)
+{
+	while (n)
+	{
+		if (n & 1)
+			_putchar('1');
+		else if (n == 0)
+			_putchar('0');
+		n = n >> 1;
+	}
+}

028bd99a7ee0e4f43f085e375e5fe93bcbbdd49a
Noel Reeds <noelreeds01@gmail.com>
2023-11-03 10:36:57 +0000

    solved betty coding style

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,14 +13,17 @@ void print_binary(unsigned long int n)
 	temp = n;
 	if (n == 0)
 		_putchar('0');
-	while ((temp >>= 1) > 0)
-		var++;
-	while (var >= 0)
+	else
 	{
-		if ((n >> var) & 1)
-			_putchar('1');
-		else
-			_putchar('0');
+		while ((temp >>= 1) > 0)
+			var++;
+		while (var >= 0)
+		{
+			if ((n >> var) & 1)
+				_putchar('1');
+			else
+				_putchar('0');
 		var--;
+		}
 	}
 }

3d0224ae854eab65e7906fb7102d84babf803afe
Noel Reeds <noelreeds01@gmail.com>
2023-11-04 00:37:06 +0000

    sort to cover upto 64 bit integers

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int i;
 	int count = 0;
 
-	for (i = 0; i < 32; i++)
+	for (i = 0; i < 64; i++)
 	{
 		if (((n >> i) & 1) != ((m >> i) & 1))
 			count++;

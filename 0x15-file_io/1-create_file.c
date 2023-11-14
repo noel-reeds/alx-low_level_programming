00f78ed05abf0b6be6da52286bb0e43dc413e3b4
Noel Reeds <noelreeds01@gmail.com>
2023-11-14 23:35:16 +0000

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,20 +11,22 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len2 = 0, nbyte = 0;
-	char *buf = NULL;
 
 	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		return(-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
 		len2 = strlen(text_content);
-		nbyte = write(fd, buf, len2);
+		nbyte = write(fd, text_content, len2);
+		if (nbyte == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-	if (nbyte == -1)
-		return (-1);
 	close(fd);
 	return (1);
 }

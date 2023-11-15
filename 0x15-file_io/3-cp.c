6717b61088887f3eec69783881ee5422dd650c3c
Noel Reeds <noelreeds01@gmail.com>
2023-11-15 14:10:05 +0000

    copy from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
-  * main - copies file_from to file_to.
+  * cp_file_from_file_to- copies file_from to file_to.
   *
-  *@argc: Number of arguments passed
+  *@file_from: file to copy from.
   *
-  *@argv: argument array.
+  *@file_to: file to copy to.
   *
   *Return: Always 1 on success and -1 on error.
   */
 int cp_file_from_file_to(char *file_from, char *file_to)
 {
-	int fd, fd2, nbyte, m, n, argc = 0;
+	int fd, nbyte, m, argc = 0;
 	char buf[1024];
 
 	if (argc != 2)
@@ -23,12 +23,6 @@ int cp_file_from_file_to(char *file_from, char *file_to)
 		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
-	fd2 = open(file_from, O_RDONLY);
-	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
-		exit(98);
-	}
 	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
@@ -36,9 +30,8 @@ int cp_file_from_file_to(char *file_from, char *file_to)
 		exit(99);
 	}
 	nbyte = write(fd, buf, 1024);
-	n = close(fd);
-	m = close(fd2);
-	if (n == -1 || m == -1)
+	m = close(fd);
+	if (m == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
 		exit(100);

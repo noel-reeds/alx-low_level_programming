a582eb40a9ca7025c1bfd3d2c6180313f158fced
Noel Reeds <noelreeds01@gmail.com>
2023-11-16 07:15:13 +0000

    Program can now copy text file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,37 +1,47 @@
 #include "main.h"
 /**
-  * cp_file_from_file_to- copies file_from to file_to.
+  * main - copies file_from to file_to.
   *
-  *@file_from: file to copy from.
+  *@argc: argument count.
   *
-  *@file_to: file to copy to.
+  *@argv: argument array.
   *
   *Return: Always 1 on success and -1 on error.
   */
-int cp_file_from_file_to(char *file_from, char *file_to)
+int main(int argc, char *argv[])
 {
-	int fd, nbyte, m, argc = 0;
-	char buf[1024];
+	int fd, fd2, m, n;
+	char buf[BUF_SIZE];
+	int nbyte;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (!file_from)
+	fd2 = open(argv[1], O_RDONLY);
+	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
-	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
 	}
-	nbyte = write(fd, buf, 1024);
+	while ((nbyte = read(fd2, buf, BUF_SIZE)) > 0)
+		if (write(fd, buf, nbyte) != nbyte)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
+			close(fd);
+			close(fd2);
+			exit(99);
+		}
 	m = close(fd);
-	if (m == -1)
+	n = close(fd2);
+	if (m == -1 || n == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
 		exit(100);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUF_SIZE 1024
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -13,6 +14,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int cp_file_from_file_to(char *file_from, char *file_to);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN */

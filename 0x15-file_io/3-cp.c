7b8d3056d034238432dbe65dc5bfbb07c9132515
Noel Reeds <noelreeds01@gmail.com>
2023-11-16 13:04:14 +0000

    correct argument vector

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	fd2 = open(argv[1], O_RDONLY);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);

ad0daab1423b9df8ae7725a665d32c34bf51099c
Noel Reeds <noelreeds01@gmail.com>
2023-11-17 12:29:45 +0000

    sorts to customize readelf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _readelf - reads and displays contents of a ELF header.
+ *
+ *@args: number of arguments.
+ *
+ *@argv: argument array.
+ *
+ *Return: ELF file format or exit(98) on error.
+ */
+int _readelf(int args, char *argv[])
+{
+	int fd, num;
+
+	if (args != 2)
+	{
+		dprintf(stderr, "Error: Not an ELF file\n");
+		exit(98);
+	}
+}

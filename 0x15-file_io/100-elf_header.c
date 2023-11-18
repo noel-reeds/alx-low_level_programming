a5c61a3b5a25e601f9ded3ca7c1a02aa60c5a031
Noel Reeds <noelreeds01@gmail.com>
2023-11-18 22:30:02 +0000

    still sorting

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -9,9 +9,10 @@
  *
  *Return: ELF file format or exit(98) on error.
  */
-int _readelf(int args, char *argv[])
+int main(int args, char *argv[])
 {
-	int fd, num;
+	int fd, num2, num;
+	char *buff[52];
 
 	if (args != 2)
 	{
@@ -21,5 +22,19 @@ int _readelf(int args, char *argv[])
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		fprintf(stderr, "%s", "Error: Failed to open ELF file\n");
-	
+	lseek(fd, 52, SEEK_SET);
+	num = read(fd, buff, 52);
+	if (num == -1)
+	{
+		fprintf(stderr, "%s", "Permission denied\n");
+		exit(98);
+	}
+	num2 = write(STDOUT_FILENO, buff, num);
+	if (num2 == -1)
+	{
+		fprintf(stderr, "%s", "Failed to write\n");
+		exit(98);
+	}
+	close(fd);
+	return (num2);
 }

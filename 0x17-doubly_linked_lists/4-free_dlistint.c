614cef2dce3d0793f0fb239d94186a45a1371b31
Noel Reeds <noelreeds01@gmail.com>
2023-12-07 23:20:33 +0000

    Frees a doubly linkedlist

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a linkedlist
+ *
+ *@head: head pointer
+ *
+ *Return: Always 0.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	while (temp != NULL)
+	{
+		dlistint_t *tp = temp;
+		temp = temp->next;
+		free(tp);
+	}
+}

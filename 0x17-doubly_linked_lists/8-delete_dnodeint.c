ec3b75c04fa4ee7ba72f47919736b26b8ecbe731
Noel Reeds <noelreeds01@gmail.com>
2023-12-08 01:53:15 +0000

    deletes a node at an index

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delets a node at an index.
+ *
+ *@head: head pointer.
+ *
+ *@index: index of node to be deleted.
+ *
+ *Return: Always 1 for success and -1 for failure.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *delnode = *head;
+
+	if (*head == NULL)
+	{
+		return (1);
+	}
+	if (index == 0)
+	{
+		*head = delnode->next;
+		if (*head != NULL)
+		{
+			delnode->next->prev = NULL;
+		}
+		free(delnode);
+		delnode = NULL;
+		return (1);
+	}
+	while (i < index)
+	{
+		delnode = delnode->next;
+		i++;
+	}
+	delnode->prev->next = delnode->next;
+	delnode->next->prev = delnode->prev;
+	free(delnode);
+	delnode = NULL;
+	return (1);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,5 +27,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 size_t dlistint_len(const dlistint_t *h);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /*----LISTS_H----*/

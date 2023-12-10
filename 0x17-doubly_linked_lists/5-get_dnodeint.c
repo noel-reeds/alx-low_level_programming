452d79bd1d758e5bea3089f87b40e8ee833b6792
Noel Reeds <noelreeds01.com>
2023-12-10 17:57:33 +0000

    Fixed segfault and retrieve the required node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -19,6 +19,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
         if (count == index)
             return (iter);
+        count++;
 		iter = iter->next;
 	}
     return (iter);

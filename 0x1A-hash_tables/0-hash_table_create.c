0dd0dc2f905ef26f36ccfac621f03f1869bda8ea
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-02 11:52:56 +0000

    allocated cont. alloc and initialized items

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,10 +8,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
 	if (!hash_table)
 		return (NULL);
 	hash_table->size = size;
+	hash_table->array = calloc(hash_table->size, sizeof(hash_node_t));
+	for (i = 0; i < hash_table->size; i++)
+		hash_table->array[i] = NULL;
 	return (hash_table);
 }

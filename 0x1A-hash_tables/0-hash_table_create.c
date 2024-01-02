ff14dd8d1952aa22588af694ab7e1051cacc0b87
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-02 12:34:04 +0000

    readjusted calloc args

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!hash_table)
 		return (NULL);
 	hash_table->size = size;
-	hash_table->array = calloc(hash_table->size, sizeof(hash_node_t*));
+	hash_table->array = calloc(hash_table->size, sizeof(hash_node_t *));
 	for (i = 0; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
 	return (hash_table);

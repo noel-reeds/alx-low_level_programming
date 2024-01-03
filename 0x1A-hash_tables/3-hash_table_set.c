2317005471242fa26a6978bbe85780262e88d470
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-01-03 11:17:19 +0000

    Fixed betty coding style

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,31 +13,23 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash, index;
-	hash_node_t **array;
-	hash_node_t temp;
-	/*..create an node of a hash table to be inserted.*/
+	hash_node_t **array, temp;
 	hash_node_t *Node = malloc(sizeof(hash_node_t));
-	/*..create a linked list for chaining.*/
 	hash_node_t *head_ptr = malloc(sizeof(hash_node_t));
 
 	if (!Node)
 		return (0);
-	/*..assign key and value of Node.*/
 	Node->key = key;
 	Node->value = value;
-	/*..compute an index.*/
 	index = key_index(key);
 	if (index)
 	{
-		/*..check if keys are same and update value if False*/
 		if (strcmp(ht->array[index]->key, Node->key))
 		{
-			/*..handle collision.*/
 			if (head_ptr)
 			{
-				/*..iterate thro. the linked list.*/
 				temp = head_ptr;
-				while(temp->next)
+				while (temp->next)
 					temp = temp->next;
 				temp->next = Node;
 				Node->next = NULL;
@@ -46,11 +38,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			head_ptr = Node;
 			Node->next = NULL;
 		}
-		/*..update Node value.*/
 		ht->array[index]->value = value;
-		return
+		return;
 	}
-	/*..insert into the array.*/
 	ht->array[index] = Node;
 	return (Node);
 }

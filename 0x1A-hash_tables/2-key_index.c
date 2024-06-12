729b83cb25a04784d7bf576fb4e7bbed532a4b11
altoid <noelreeds01@gmail.com>
2024-06-12 04:22:01 +0000

    creates an index from a given key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,6 +13,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int index, hash;
 
 	hash = hash_djb2(key);
-	index = hash % size; /*..creates an index withing the array size.*/
+	index = hash % size; /*..creates an index..*/
 	return (index);
 }

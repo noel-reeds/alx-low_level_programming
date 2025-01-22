0f816682d7c15eff34fc37192f829c91404de43c
altoid <noelreeds01@gmail.com>
2025-01-22 22:38:43 +0000

    generates random valid passwords for a crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - provides a passwd for crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int psswd, r_num;
+
+	psswd = 2772;
+	srand(time(NULL));
+	while (psswd > 119)
+	{
+		r_num = (rand() % 119);
+		printf("%c", r_num);
+		psswd -= r_num;
+	}
+	printf("%c", psswd);
+	return (0);
+}
